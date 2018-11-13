849c4c4c5771710f88fc155d93ad1e40334c75f0
shouwang <312434601@qq.com>
2018-11-13 15:14:37 +0800

    第一次测试修改提交

---
+++ src/components/login/login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+       <el-form>
+           <el-form-item label="用户名" prop="pass">
+               
+           </el-form-item>
+        </el-form> 
+    </div>
+    
+</template>
+<script>
+</script>
+<style lang="">
+</style>
